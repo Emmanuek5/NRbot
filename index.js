23c1ca1faef789defda5e1f0b4f63dc52456e79a
Emmanuek5 <83721168+Emmanuek5@users.noreply.github.com>
2022-06-09 19:47:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = ['forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
@@ -15,12 +15,12 @@ var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
-	
+
   host: host,
   username: username
 });
 function getRandomArbitrary(min, max) {
-       return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 
 }
 
@@ -28,49 +28,76 @@ bot.loadPlugin(cmd)
 
 
 
-bot.on('login',function(){
-	console.log("Logged In")
-	bot.chat("hello");
-	
+bot.on('login', function() {
+  console.log("Logged In")
+  bot.chat("andrew");
+
 });
 
 bot.on('time', function(time) {
-	if(nightskip == "true"){
-	if(bot.time.timeOfDay >= 13000){
-	bot.chat('/time set day')
-	}}
-    if (connected <1) {
-        return;
+  if (nightskip == "true") {
+    if (bot.time.timeOfDay >= 13000) {
+      bot.chat('/time set day')
     }
-    if (lasttime<0) {
-        lasttime = bot.time.age;
+  }
+  if (connected < 1) {
+    return;
+  }
+ 
+    if (lasttime < 0) {
+      lasttime = bot.time.age;
     } else {
-        var randomadd = Math.random() * maxrandom * 20;
-        var interval = moveinterval*20 + randomadd;
-        if (bot.time.age - lasttime > interval) {
-            if (moving == 1) {
-                bot.setControlState(lastaction,false);
-                moving = 0;
-                lasttime = bot.time.age;
-            } else {
-                var yaw = Math.random()*pi - (0.5*pi);
-                var pitch = Math.random()*pi - (0.5*pi);
-                bot.look(yaw,pitch,false);
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                moving = 1;
-                lasttime = bot.time.age;
-                bot.activateItem();
-            }
+      var randomadd = Math.random() * maxrandom * 20;
+      var interval = moveinterval * 20 + randomadd;
+      if (bot.time.age - lasttime > interval) {
+        if (moving == 1) {
+          bot.setControlState(lastaction, false);
+          moving = 0;
+          lasttime = bot.time.age;
+        } else {
+          var yaw = Math.random() * pi - (0.5 * pi);
+          var pitch = Math.random() * pi - (0.5 * pi);
+          bot.look(yaw, pitch, false);
+          lastaction = actions[Math.floor(Math.random() * actions.length)];
+          bot.setControlState(lastaction, true);
+          moving = 1;
+          lasttime = bot.time.age;
+          bot.activateItem();
         }
+      }
     }
+  
 });
 
-bot.on('spawn',function() {
-    connected=1;
+bot.on('spawn', function() {
+  connected = 1;
 });
 
-bot.on('death',function() {
-    bot.emit("respawn")
+bot.on('death', function() {
+  bot.chat("Why \n God \n I Lost My stuff")
+  bot.emit("respawn")
 });
-
+bot.on('kicked', console.log)
+bot.on('error', console.log)
+bot.on('end', function () {
+    console.log("Disconnected. Waiting 10 seconds")
+    bot.quit();
+  lasttime = -1;
+    moving = 0;
+    connected=0;
+    bot = mineflayer.createBot({
+        host: host,      
+        username: username
+    });
+   
+  if(bot){
+     console.log("reconnected.")
+  }
+});
+const express = require("express")
+const app = express()
+app.set("view-engine", "ejs")
+app.get("/", (req, res) => {
+  res.render("app.ejs")
+})
+app.listen("8808")
